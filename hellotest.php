4f020e2fbcc7b96b18c3ed4a976c38b5c22bd399
kei <>
2018-09-26 16:36:27 +0900

    mysql test

--- hellotest.php
+++ hellotest.php
@@ -1,11 +1,14 @@
 <?php
 
 $link = mysqli_connect("localhost","root","password");
-mysqli_query("create database if not exists test2 default character set utf8");
-$result = mysqli_query("show databases");
 
-while ($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
-    echo $row[0];
-    echo "<br>";
-}
+echo $link->info;
+
+//mysqli_query("create database if not exists test2 default character set utf8");
+//$result = mysqli_query("show databases");
+//
+//while ($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
+//    echo $row[0];
+//    echo "<br>";
+//}
 ?>
