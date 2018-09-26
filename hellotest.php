342cbc4c6fd51fc980d464498806c8b5e4878cb9
kei <>
2018-09-26 16:13:15 +0900

    mysql test

--- hellotest.php
+++ hellotest.php
@@ -1,7 +1,11 @@
-<?php echo '<p>Hello World</p>'; ?>
-/**
- * Created by PhpStorm.
- * User: kkatou
- * Date: 2018/09/26
- * Time: 15:26
- */
+<?php
+
+$link = mysqli_connect("localhost","root","password");
+mysqli_query("create database if not exists test2 default character set utf8");
+$result = mysqli_query("show databases");
+
+while ($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
+    echo $row[0];
+    echo "<br>";
+}
+?>
