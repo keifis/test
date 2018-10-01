52784ce6ee742348ff484218cf7bfe889e3acd58
kei <>
2018-10-01 15:11:03 +0900

    test

--- hellotest.php
+++ hellotest.php
@@ -19,13 +19,13 @@ echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
 //echo "hello php";
 
 //mysqli_query("create database if not exists test2 default character set utf8");
-$result = mysqli_query("SHOW DATABASES;");
+$result = mysqli_query("SHOW DATABASES");
 
-//if (!$result) {
-//    echo "DB Error, could not list tables\n";
-//    echo 'MySQL Error: ' . mysqli_error($link);
-//    exit;
-//}
+if (!$result) {
+    echo "DB Error, could not list tables\n";
+    echo 'MySQL Error: ' . mysqli_error($link);
+    exit;
+}
 //
 var_dump($result);
 //
